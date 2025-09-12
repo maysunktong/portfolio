00e96b6adefae638ca8fa6104cbf8b2d18ef7476
maysunktong <pattaranan.sunktong@gmail.com>
2025-09-12 18:26:26 +0200

    Add social network

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,6 +1,6 @@
 import ProfileImage from "./ui/ProfileImage";
 import MenuButton from "./ui/MenuButton";
-import { Mail } from "lucide-react";
+import { Mail, Gamepad, Linkedin } from "lucide-react";
 import { StatusIndicator } from "./ui/StatusIndicator";
 
 export default function HeroBanner() {
@@ -20,19 +20,42 @@ export default function HeroBanner() {
           Frontend Dev Student
         </h2>
         <p className="text-sm md:text-lg text-[#767676] max-w-xl md:max-w-2xl py-2 leading-6">
-          Hi there!I am a web development student based in Stockholm with a passion for creating user-friendly and creative websites. I love turning ideas into clean, responsive interfaces as I have a strong eye for design.
-         
+          Hi there!I am a web development student based in Stockholm with a
+          passion for creating user-friendly and creative websites. I love
+          turning ideas into clean, responsive interfaces as I have a strong eye
+          for design.
         </p>
-        <p className="max-w-xl md:max-w-2xl font-bold"> I am looking for a 5-month long internship from <span className="underline">January to May 2026 </span>(can start earlier if needed)
-           Thanks for stopping by — please checkout my work below!</p>
+        <p className="max-w-xl md:max-w-2xl font-bold">
+          {" "}
+          I am looking for a 5-month long internship from{" "}
+          <span className="underline">January to May 2026 </span>(can start
+          earlier if needed) Thanks for stopping by — please checkout my work
+          below!
+        </p>
       </div>
-      <MenuButton
-        href="mailto:suras.sunktong@gmail.com"
-        label="Contact me"
-        bg="black"
-        textColor="white"
-        icon={<Mail />}
-      />
+      <div className="flex gap-2">
+        <MenuButton
+          href="mailto:suras.sunktong@gmail.com"
+          label="Email me"
+          bg="black"
+          textColor="white"
+          icon={<Mail />}
+        />
+        <MenuButton
+          href="https://discord.gg/ss7tn39GPJ"
+          label="Discord"
+          bg="black"
+          textColor="white"
+          icon={<Gamepad />}
+        />
+        <MenuButton
+          href="https://www.linkedin.com/in/may-sunktong-276b53143/"
+          label="LinkedIn"
+          bg="black"
+          textColor="white"
+          icon={<Linkedin />}
+        />
+      </div>
     </section>
   );
 }
--- components/ui/MenuButton.tsx
+++ components/ui/MenuButton.tsx
@@ -11,6 +11,7 @@ export default function Menu({
 }: LinkButtonTypes) {
   return (
     <Link
+      target="_blank"
       href={href}
       className={`w-fit flex gap-2 justify-center items-center bg-${bg}  border-1 text-${textColor} font-mono px-6 py-3 rounded-lg shadow-[4px_4px_0_0_rgba(0,0,0,1)] active:translate-y-1 active:shadow-sm transition-transform duration-150 dark:bg-transparent dark:text-white dark:border-white hover:border-dashed`}
     >
