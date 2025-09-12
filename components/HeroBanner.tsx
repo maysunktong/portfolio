da4a2b3601592e84e95d3e11b09c89271db3249d
maysunktong <pattaranan.sunktong@gmail.com>
2025-09-12 11:59:01 +0200

    fix errors

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -10,11 +10,8 @@ export default function HeroBanner() {
       <h1 className="text-7xl font-semibold">May Sunktong</h1>
       <h2 className="text-7xl font-semibold">Frontend Student</h2>
       <p className="font-thin text-gray-500">
-        Based in London, I specialize in capturing authentic moments and turning
-        them into visual stories. My work has been featured in GQ Magazine, and
-        I've collaborated with clients ranging from families to global brands.
+       subtitle
       </p>
-
       <button type="button" className="bg-black">
         <Link href="/" className="text-white">
           Contact me
--- components/ui/ProfileImage.tsx
+++ components/ui/ProfileImage.tsx
@@ -1,5 +1,3 @@
-import { StatusIndicator } from "./StatusIndicator";
-
 export default function ProfileImage() {
   return (
     <div className="rounded-2xl aspect-square overflow-hidden w-30">
