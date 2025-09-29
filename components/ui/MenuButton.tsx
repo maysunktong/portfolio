70804072b9134434747cc8ac5f4a32633c7ef227
May Sunktong <pattaranan.sunktong@gmail.com>
2025-09-29 15:13:08 +0200

    font color white for buttons

--- components/ui/MenuButton.tsx
+++ components/ui/MenuButton.tsx
@@ -10,7 +10,7 @@ export default function Menu({
     <Link
       target="_blank"
       href={href}
-      className={`w-fit flex gap-2 justify-center items-center bg-${bg} border-1 font-mono px-6 py-3 rounded-lg shadow-[4px_4px_0_0_rgba(0,0,0,1)] border-black active:translate-y-1 dark:border-white hover:border-dashed`}
+      className={`text-white w-fit flex gap-2 justify-center items-center bg-${bg} border-1 font-mono px-6 py-3 rounded-lg shadow-[4px_4px_0_0_rgba(0,0,0,1)] border-black active:translate-y-1 dark:border-white hover:border-dashed`}
     >
       <p>{label}</p>
     </Link>
