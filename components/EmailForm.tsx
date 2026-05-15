265e7e24fbbe98720ff790a7d9a49b23412f0779
maysunktong <pattaranan.sunktong@gmail.com>
2026-05-15 23:17:09 +0200

    Delete react element

--- components/EmailForm.tsx
+++ components/EmailForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ReactElement, useState } from "react";
+import React, { useState } from "react";
 import { X } from "lucide-react";
 
 export default function EmailForm({ onClose }: { onClose: () => void }) {
